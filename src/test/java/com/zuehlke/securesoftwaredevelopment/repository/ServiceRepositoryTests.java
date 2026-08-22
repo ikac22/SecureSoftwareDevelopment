6397294887cb8ea61ddf3281ac4705739099bfe1
eliyah <42802540+ikac22@users.noreply.github.com>
2026-08-22 18:13:51 +0200

    Isolate service repository integration tests

--- src/test/java/com/zuehlke/securesoftwaredevelopment/repository/ServiceRepositoryTests.java
+++ src/test/java/com/zuehlke/securesoftwaredevelopment/repository/ServiceRepositoryTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.sql.SQLException;
 
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
 @Import(ServiceRepository.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ServiceRepositoryTests {
 
     @Autowired
